3f1fe2b1424ddcbcda2929263be61dbbecd51cf3
Ömer Hamid Kamışlı <ohkamisli@Omer-MacBook-Pro.local>
2020-12-11 13:58:01 +0300

    Typo fixed

--- src/components/alert/alert.js
+++ src/components/alert/alert.js
@@ -5,14 +5,7 @@ const createAlert = async (title, message) => {
   Alert.alert(
     title,
     message,
-    [
-      {
-        text: "Cancel",
-        onPress: () => console.log("Cancel Pressed"),
-        style: "cancel",
-      },
-      { text: "OK", onPress: () => console.log("OK Pressed") },
-    ],
+    [{ text: "Tamam", onPress: () => console.log("OK Pressed") }],
     { cancelable: false }
   );
   return;
